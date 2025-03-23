01f949e06aefb8eacee0628a55f67a3c1632a30f
Kseniia Tishchenko <k.tishchenkox@gmail.com>
2025-03-23 18:56:48 +0400

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// import Question from './entities/Question/Question.vue'
+</script>
+
+<template>
+  <!-- <Question :question="{
+    title: 'hii',
+    content: '<b>Question Content',
+    answers: [{id: '1', content: 'answer 1'}],
+    correctAnswer: '1',
+  }" /> -->
+</template>
+
+<style scoped>
+</style>
