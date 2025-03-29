b863de8be0441b9fd07432582469977b90240ac1
Kseniia Tishchenko <kseniia_tishchenko@fastmail.com>
2025-03-29 20:40:34 +0400

    build: add test and lint commands

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+// eslint.config.mjs
+import pluginVue from "eslint-plugin-vue";
+import {
+  defineConfigWithVueTs,
+  vueTsConfigs,
+} from "@vue/eslint-config-typescript";
+
+export default defineConfigWithVueTs(
+  pluginVue.configs["flat/essential"],
+  vueTsConfigs.recommended,
+  {
+    rules: {
+      "vue/multi-word-component-names": 0,
+    },
+  }
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -8,4 +8,8 @@ export default defineConfig({
     vue(), 
     tailwindcss(),
   ],
+  test: {
+    globals: true,
+    environment: 'happy-dom'
+  }
 })
